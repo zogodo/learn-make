8a62d70dad333bcc91501222ef894c0f56ad575d
zogodo <742782908@qq.com>
2019-07-19 18:40:43 +0800

    OK

--- automake/auto.sh
+++ automake/auto.sh
@@ -1,18 +1,5 @@
-rm -rf config.log autom4te.cache autoscan.log config.h.in config.status configure aclocal.m4 COPYING INSTALL depcomp install-sh missing Makefile Makefile.in config.h 
-#autoscan
-#mv configure.scan configure.ac
-#vim configure.ac
-
-aclocal
-autoheader
-autoconf
-#automake -a
-automake --add-missing
+rm -rf aclocal.m4 autom4te.cache config.h config.h.in config.log config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h1
+autoreconf --install
 ./configure
-make && make install
-
-
-#AC_INIT([automake], [1.0], [zgd@qq.com])
-#AM_INIT_AUTOMAKE([foreign -Werror])
-
+make
 
