3857295f65acc60ec509aa1c7e37567ff4c04f6f
zogodo <742782908@qq.com>
2019-07-29 17:40:00 +0800

    完全清理

--- automake/auto.sh
+++ automake/auto.sh
@@ -1,4 +1,5 @@
 rm -rf aclocal.m4 autom4te.cache config.h config.h.in config.log config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h1
+rm -rf */*.o */*.in */Makefile */Sub*
 autoreconf --install
 ./configure
 make
