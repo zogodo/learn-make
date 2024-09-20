842a936ea965b3b60a0181a5d1adf28ffb473b1c
root <742782908@qq.com>
2024-09-20 15:44:28 +0800

    auto **.c to **.o OK

--- Makefile/auto_c2o.mk
+++ Makefile/auto_c2o.mk
@@ -1,13 +1,15 @@
 FLAGS = -I a/ -I b/
 CFLAGS=${FLAGS}
-CPPFLAGS=-std=c++11 ${FLAGS}
+CPPFLAGS=${FLAGS}
+# CPPFLAGS=-std=c++11 ${FLAGS}
+CC=gcc
 
 SRCS=$(wildcard *.c a/*.c b/*.c b/bb/*.c)#把所有路径的.c文件全部展开
 SRCS2=$(notdir $(SRCS))#去掉所有路径
-OBJS=$(patsubst %.c,%.o,$(SRCS2) )#替换.c成.o
+OBJS=$(patsubst %.c,%.o,$(SRCS) )#替换.c成.o
 
-all: $(SRCS2)
+all: $(OBJS)
 	@echo $(SRCS)
 	@echo $(SRCS2)
 	@echo $(OBJS)
-
+	$(CC) -o xxx $(OBJS)
