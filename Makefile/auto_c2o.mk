8ab3874fe827788c42384d12ed0f8c6c3301939d
root <742782908@qq.com>
2024-09-20 16:05:33 +0800

    up

--- Makefile/auto_c2o.mk
+++ Makefile/auto_c2o.mk
@@ -8,8 +8,20 @@ SRCS=$(wildcard *.c a/*.c b/*.c b/bb/*.c)#把所有路径的.c文件全部展开
 SRCS2=$(notdir $(SRCS))#去掉所有路径
 OBJS=$(patsubst %.c,%.o,$(SRCS) )#替换.c成.o
 
+# SRCS=x.c main.c a/a.c b/b.c b/bb/bb.c
+# SRCS2=x.c main.c a.c b.c bb.c
+# OBJS=x.o main.o a/a.o b/b.o b/bb/bb.o
+
 all: $(OBJS)
-	@echo $(SRCS)
-	@echo $(SRCS2)
-	@echo $(OBJS)
 	$(CC) -o xxx $(OBJS)
+
+clean:
+	@find . -name "*.o" | xargs rm
+
+#并不需要写 .c 到 .o 的 target
+#会自动执行以下命令:
+# gcc -std=c99 -I. -Ia/ -Ib/   -c -o x.o x.c
+# gcc -std=c99 -I. -Ia/ -Ib/   -c -o main.o main.c
+# gcc -std=c99 -I. -Ia/ -Ib/   -c -o a/a.o a/a.c
+# gcc -std=c99 -I. -Ia/ -Ib/   -c -o b/b.o b/b.c
+# gcc -std=c99 -I. -Ia/ -Ib/   -c -o b/bb/bb.o b/bb/bb.c
