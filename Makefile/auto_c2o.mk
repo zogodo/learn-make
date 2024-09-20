e661131d775957c0ac0c18619ad6ccb0ea43eee8
root <742782908@qq.com>
2024-09-20 15:56:05 +0800

    up

--- Makefile/auto_c2o.mk
+++ Makefile/auto_c2o.mk
@@ -1,9 +1,9 @@
-FLAGS = -I a/ -I b/
-CFLAGS=${FLAGS}
-CPPFLAGS=${FLAGS}
+FLAGS = -I. -Ia/ -Ib/
+CFLAGS=-std=c99 ${FLAGS}
 # CPPFLAGS=-std=c++11 ${FLAGS}
 CC=gcc
 
+#注释的#号前面有空格的话,前面的变量最后就会有空格
 SRCS=$(wildcard *.c a/*.c b/*.c b/bb/*.c)#把所有路径的.c文件全部展开
 SRCS2=$(notdir $(SRCS))#去掉所有路径
 OBJS=$(patsubst %.c,%.o,$(SRCS) )#替换.c成.o
--- Makefile/main.c
+++ Makefile/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "x.h"
 
 int main(int argc, char const *argv[])
 {
